30b08b7d2be229ef09647334a17a33ba6d88adef
CodeGiorgino <andreagiorgino1@gmail.com>
2024-02-12 11:05:28 +0100

    Base example

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,11 +1,22 @@
 #include "../../include/cppbc.hpp"
+#include <iostream>
 
-int main(int argc, char** argv) {
-    (void)argc;
-    (void)argv;
+#define FMT_HEADER_ONLY
 
-    float retval = bc("1 + 1");
-    (void)retval;
+#include <fmt/base.h>
+#include <fmt/core.h>
+
+int main(void) {
+    fmt::println("{:=^40}", "[ example.out ]");
+
+    try {
+        float retval = bc("1 + 1 * 2 + 3");
+        fmt::println("retval: {}", retval);
+    }
+
+    catch (bc_parse_exception& ex) {
+        std::cerr << ex.what();
+    }
 
     return 0;
 }
