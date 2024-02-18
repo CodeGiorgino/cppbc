6f63766fe2fac226428b00c49bb1a08a4775d3fb
CodeGiorgino <andreagiorgino1@gmail.com>
2024-02-18 13:09:44 +0100

    Improved error message

--- include/cppbc.hpp
+++ include/cppbc.hpp
@@ -12,14 +12,18 @@
  */
 class bc_parse_exception : public std::exception {
     public:
-        bc_parse_exception(size_t pos, std::string text) : m_pos(pos), m_text(text) {
-            m_message = fmt::format("ERROR: Cannot parse source at position {}: {}\n\0",
-                    m_pos, m_text);
+        bc_parse_exception(std::string source, size_t pos, std::string text) 
+            : m_source(source), m_pos(pos), m_text(text) {
+            m_message = fmt::format("ERROR: Cannot parse source at position {}: {}\n"
+                    "  Source: {}\n"
+                    "          {:>{}}\n\0",
+                    m_pos, m_text, m_source, "^", pos + 1);
         }
 
         char *what() { return strdup(m_message.c_str()); }
 
     private:
+        std::string m_source;
         size_t m_pos;
         std::string m_text;
         std::string m_message;
